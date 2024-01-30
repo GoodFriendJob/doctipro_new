8211c872a9974abf72f4b52cfdf34a541d7f4e76
Good Friend Job <somrosomro911@gmail.com>
2024-01-31 02:24:05 +0900

    debug

--- resources/views/doctor/doctor/doctor_pid_settings.blade.php
+++ resources/views/doctor/doctor/doctor_pid_settings.blade.php
@@ -140,13 +140,12 @@
                             $is_expired = $guichet_date < time() ? true: false;
                             $is_valid = $validate || $contenst || (!$validate && !$contenst && !$is_expired && $simulate);
 
-                            echo "<h2>".$history->pid_id." - time: ".(time()- strtotime($history->guichet_date))."</h1>";
-                            echo "<br>simulate=".$simulate;
-                            echo "<br>validate=".$validate;
-                            echo "<br>contenst=".$contenst;
-                            echo "<br>is_expired=".$is_expired;
-                            echo "<br>is_valid=".$is_valid;
-
+                            // echo "<h2>".$history->pid_id." - time: ".(time()- strtotime($history->guichet_date))."</h1>";
+                            // echo "<br>simulate=".$simulate;
+                            // echo "<br>validate=".$validate;
+                            // echo "<br>contenst=".$contenst;
+                            // echo "<br>is_expired=".$is_expired;
+                            // echo "<br>is_valid=".$is_valid;
                         @endphp
                         <tr class="{{ $is_valid ? 'bg-light':'' }}">
                             <td>
