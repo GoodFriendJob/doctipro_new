f7f4320e05ec5b762508393c7d5673f64041bb24
Good Friend Job <somrosomro911@gmail.com>
2024-02-02 01:38:14 +0900

    f

--- app/Http/Controllers/Doctor/DoctorController.php
+++ app/Http/Controllers/Doctor/DoctorController.php
@@ -220,12 +220,8 @@ class DoctorController extends Controller
             $is_contestation = !empty($history->contestation_id);
             // $guichet_date = Carbon::parse($history->guichet_date)->setTimezone(env('timezone'));
             $guichet_date = Carbon::parse($history->guichet_date);
-            $expiredDate = $guichet_date->copy()->addMinutes(30);
+            $expiredDate = $guichet_date->copy()->addMinutes(30)->addHours(13);
 
-            echo $guichet_date->timestamp;
-            echo '<br>';
-            echo Carbon::now()->setTimezone(env('timezone'))->timestamp;
-            exit;
             // $is_expired = Carbon::now(env('timezone'))->greaterThan($expiredDate);
             $is_expired = Carbon::now()->greaterThan($expiredDate);
             $is_valid = $is_validation || $is_contestation || (!$is_validation && !$is_contestation && !$is_expired && $is_simulation);
